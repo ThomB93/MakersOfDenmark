eda937d5a39629f171913c4649e1c10163471fa6
Tiffanatic <madsrynord@live.dk>
2020-10-12 11:57:07 +0200

    Services project created

---
+++ MakersOfDenmark.Services/AuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MakersOfDenmark.Services
+{
+    public class AuthService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthService(IOperationSnapshot<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+    }
+}
