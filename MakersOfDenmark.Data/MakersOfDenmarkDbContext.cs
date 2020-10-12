d130aa232096ddbb914ce6239a2bf8f0f4b8c06f
Tiffanatic <madsrynord@live.dk>
2020-10-12 12:47:40 +0200

    Added Identity.

---
+++ MakersOfDenmark.Data/MakersOfDenmarkDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MakersOfDenmark.Core.Models.Auth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MakersOfDenmark.Data
+{
+    public class MakersOfDenmarkDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public MakersOfDenmarkDbContext(DbContextOptions<MakersOfDenmarkDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+        }
+    }
+}
