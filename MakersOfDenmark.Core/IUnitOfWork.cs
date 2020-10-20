4b7bef7c7ec89daaa98a47e498a108a362b67740
ThomB93 <thomb@hotmail.dk>
2020-10-20 09:41:35 +0200

    add unit of work and repository boilerplate

---
+++ MakersOfDenmark.Core/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MakersOfDenmark.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> CommitAsync();
+    }
+}
---
+++ MakersOfDenmark.Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using MakersOfDenmark.Core;
+
+namespace MakersOfDenmark.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MakersOfDenmarkDbContext _context;
+        public UnitOfWork(MakersOfDenmarkDbContext context)
+        {
+            this._context = context;
+        }
+        
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
