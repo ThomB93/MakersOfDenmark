7ec1536aee7f0eb5c86125c762d91cb5cebd51ef
Tiffanatic <madsrynord@live.dk>
2020-10-20 14:57:03 +0200

    Fixed dbcontext with migrations

--- MakersOfDenmark.Data/Migrations/20201020121545_ForeignKeyOnMakerspace.Designer.cs
+++ MakersOfDenmark.Data/Migrations/20201020121545_ForeignKeyOnMakerspace.Designer.cs
@@ -4,14 +4,16 @@ using MakersOfDenmark.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MakersOfDenmark.Data.Migrations
 {
     [DbContext(typeof(MakersOfDenmarkDbContext))]
-    partial class MakersOfDenmarkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201020121545_ForeignKeyOnMakerspace")]
+    partial class ForeignKeyOnMakerspace
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
